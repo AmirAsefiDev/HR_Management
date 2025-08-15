2054073c7565f8f5b6a43440a7b217bb82d44908
AmirAsefiDev <amirasefi.info@gmail.com>
2025-08-15 14:29:24 +0330

    Add Custom Exception.

---
+++ HR_Management.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation.Results;
+
+namespace HR_Management.Application.Exceptions;
+
+public class ValidationException : Exception
+{
+    public ValidationException(ValidationResult validationResult)
+    {
+        foreach (var error in validationResult.Errors) Errors.Add(error.ErrorMessage);
+    }
+
+    public List<string> Errors { get; set; } = new();
+}
