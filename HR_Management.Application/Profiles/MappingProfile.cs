753e83f229ee0b8fa54e9e3e8eb4a9d10f22a690
AmirAsefiDev <amirasefi.info@gmail.com>
2025-08-15 15:57:16 +0330

    Add Essential DTOs to MappingProfile for adapting them.

--- HR_Management.Application/Profiles/MappingProfile.cs
+++ HR_Management.Application/Profiles/MappingProfile.cs
@@ -10,9 +10,28 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
-        CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+        #region LeaveRequest Mapping
+
         CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
         CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+        CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+        CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+
+        #endregion
+
+        #region LeaveAllocation Mapping
+
+        CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+        CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+        CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+
+        #endregion
+
+        #region LeaveType Mapping
+
+        CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+        CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
+
+        #endregion
     }
 }
