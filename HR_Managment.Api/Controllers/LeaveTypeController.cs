e3efbe75e6a55b89b5697bf5636573dc3ca80182
AmirAsefiDev <amirasefi.info@gmail.com>
2025-08-22 21:36:01 +0330

    Add Complete CRUD of LeaveType in LeaveTypeController. Install And Configure Seq, it's a log with serilog.

---
+++ HR_Managment.Api/Controllers/LeaveTypeController.cs
@@ -0,0 +1,69 @@
+﻿using HR_Management.Application.DTOs.LeaveType;
+using HR_Management.Application.Features.LeaveTypes.Requests.Commands;
+using HR_Management.Application.Features.LeaveTypes.Requests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR_Management.Api.Controllers;
+
+[Route("api/leave-type")]
+[ApiController]
+public class LeaveTypeController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public LeaveTypeController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    // GET: api/<LeaveTypeController>
+    [HttpGet]
+    public async Task<ActionResult<List<LeaveTypeDto>>> Get()
+    {
+        var leaveTypes = await _mediator.Send(new GetLeaveTypeListRequest());
+        return Ok(leaveTypes);
+    }
+
+    // GET api/<LeaveTypeController>/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<LeaveTypeDto>> Get(int id)
+    {
+        var leaveType = await _mediator.Send(new GetLeaveTypeDetailRequest { Id = id });
+        return Ok(leaveType);
+    }
+
+    // POST api/<LeaveTypeController>
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] CreateLeaveTypeDto leaveType)
+    {
+        var command = new CreateLeaveTypeCommand
+        {
+            LeaveTypeDto = leaveType
+        };
+        var response = await _mediator.Send(command);
+        return Ok(response);
+    }
+
+    // PUT api/<LeaveTypeController>/5
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Put(int id, [FromBody] LeaveTypeDto leaveType)
+    {
+        leaveType.Id = id;
+        var command = new UpdateLeaveTypeCommand
+        {
+            LeaveTypeDto = leaveType
+        };
+        await _mediator.Send(command);
+        return NoContent();
+    }
+
+    // DELETE api/<LeaveTypeController>/5
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        var command = new DeleteLeaveTypeCommand { Id = id };
+        await _mediator.Send(command);
+        return NoContent();
+    }
+}
--- HR_Managment.Api/Program.cs
+++ HR_Managment.Api/Program.cs
@@ -1,6 +1,7 @@
 using HR_Management.Application;
 using HR_Management.Infrastructure;
 using HR_Management.Persistence;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,17 @@ builder.Services.ConfigureApplicationServices();
 builder.Services.ConfigurePersistenceServices(builder.Configuration);
 builder.Services.ConfigureInfrastructureServices(builder.Configuration);
 
+// config Serilog
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.Seq("http://localhost:5341") //SEQ Address
+    .CreateLogger();
+
+//replace default ASP.NET Core logger with Serilog
+builder.Host.UseSerilog((ctx, lc) =>
+    lc.ReadFrom.Configuration(ctx.Configuration));
+
 builder.Services.AddCors(o =>
 {
     o.AddPolicy("CorsPolicy",
@@ -21,9 +33,14 @@ builder.Services.AddCors(o =>
                 .AllowAnyMethod());
 });
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseSerilogRequestLogging(); // Enable Serilog request/response logging
 
 app.UseHttpsRedirection();
 
